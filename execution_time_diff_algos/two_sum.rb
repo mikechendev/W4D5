b743e0eb0c38b4c1063392960d82cae6d4ac0e48
Michael Chen <michael.chen.1120@gmail.com>
2022-04-22 17:02:39 -0400

    Fix spacing

--- execution_time_diff_algos/two_sum.rb
+++ execution_time_diff_algos/two_sum.rb
@@ -1,42 +1,39 @@
 require 'byebug'
 
 def bad_two_sum?(arr, target)
-      (0...arr.length - 1).each do |i|
-            (i+1...arr.length).each do |k|
-                  return true if arr[i] + arr[k] == target
-            end
-      end
-      return false
+    (0...arr.length - 1).each do |i|
+        (i+1...arr.length).each do |k|
+            return true if arr[i] + arr[k] == target
+        end
+    end
+    return false
 end
 # O(n^2)
 
 
 def okay_two_sum?(arr, target)
-      arr.sort
-      l = 0
-      r = arr.length - 1
-      while (arr[l] < arr[r])
-            if arr[l] + arr[r] == target
-                  return true
-            elsif arr[l] + arr[r] < target
-                  l += 1
-            else 
-                  r -= 1
-            end
-      end
-      return false
+    arr.sort
+    l = 0
+    r = arr.length - 1
+    while (arr[l] < arr[r])
+        if arr[l] + arr[r] == target
+              return true
+        elsif arr[l] + arr[r] < target
+              l += 1
+        else 
+              r -= 1
+        end
+    end
+    return false
 end
 
 def two_sum?(arr, target)
-      #debugger
-      hash = {}
-
-      arr.each do |num|
-            return true if hash[target - num]
-            hash[num] = true
-      end
-
-      return false
+    hash = {}
+    arr.each do |num|
+        return true if hash[target - num]
+        hash[num] = true
+    end
+    return false
 end
 
 # O(logn)
--- execution_time_diff_algos/windowed.rb
+++ execution_time_diff_algos/windowed.rb
@@ -17,57 +17,58 @@ end
 # p windowed_max_range([1, 3, 2, 5, 4, 8], 5) #== 6 # 3, 2, 5, 4, 8
 
 class MyQueue
-  def initialize
-    @store = []
-  end
+    def initialize
+        @store = []
+    end
 
-  def enqueue(val)
-    @store.push(val)
-  end
+    def enqueue(val)
+        @store.push(val)
+    end
 
-  def dequeue
-    @store.shift
-  end
+    def dequeue
+        @store.shift
+    end
 
-  def empty?
-    @store.empty?
-  end
+    def empty?
+        @store.empty?
+    end
 
-  def size
-    @store.length
-  end
+    def size
+        @store.length
+    end
 
-  def peek
-    @store.first
-  end
+    def peek
+        @store.first
+    end
+
 end
 
 class MyStack
-  def initialize
-    @store = []
-  end
+    def initialize
+        @store = []
+    end
 
-  def push(val)
-    @store.push(val)
-  end
+    def push(val)
+        @store.push(val)
+    end
 
-  def pop
-    @store.pop
-  end
+    def pop
+        @store.pop
+    end
 
-  def empty?
-    @store.empty?
-  end
+    def empty?
+        @store.empty?
+    end
 
-  def size
-    @store.length
-  end
+    def size
+        @store.length
+    end
 
-  def peek
-    @store.last
-  end
+    def peek
+        @store.last
+    end
 
-  private
+    private
 end
 
 class StackQueue
@@ -88,19 +89,19 @@ class StackQueue
     end
 
     def dequeue
-      @stack.pop
+        @stack.pop
     end
 
     def empty?
-      @stack.empty?
+        @stack.empty?
     end
 
     def size
-      @stack.length
+        @stack.length
     end
 
     def peek
-      @stack.first
+        @stack.first
     end
 end
 
@@ -109,110 +110,112 @@ end
 
 class MinMaxStack
 
-  def initialize
-    @stack = MyStack.new
-  end
+    def initialize
+        @stack = MyStack.new
+    end
 
-  def push(val)
-    @stack.push({ maximum: check_max(val), minimum: check_min(val), value: val })
-  end
+    def push(val)
+        @stack.push({ maximum: check_max(val), minimum: check_min(val), value: val })
+    end
 
-  def pop
-    @stack.pop[:value]
-  end
-  
-  def peek
-    @stack.peek[:value] 
-  end
+    def pop
+        @stack.pop[:value]
+    end
 
-  def size
-    @stack.size
-  end
+    def peek
+        @stack.peek[:value] 
+    end
 
-  def empty?
-    @stack.empty?
-  end
+    def size
+        @stack.size
+    end
 
-  def max
-    @stack.peek[:maximum] 
-  end
+    def empty?
+        @stack.empty?
+    end
 
-  def min
-    @stack.peek[:minimum] 
-  end
-  private 
-  def check_max(val)
-    empty? ? val : [max, val].max
-  end
+    def max
+        @stack.peek[:maximum] 
+    end
 
-  def check_min(val)
-    empty? ? val : [min, val].min
-  end
+    def min
+        @stack.peek[:minimum] 
+    end
+
+    private
+
+    def check_max(val)
+        empty? ? val : [max, val].max
+    end
+
+    def check_min(val)
+        empty? ? val : [min, val].min
+    end
 end
 
 class MinMaxStackQueue
-  def initialize
-    @stack = MinMaxStack.new
-    @stack_2 = MinMaxStack.new
-  end
-
-  def enqueue(val)
-    while !@stack.empty?
-       @stack_2.push(@stack.pop)
+    def initialize
+        @stack = MinMaxStack.new
+        @stack_2 = MinMaxStack.new
     end
-    @stack.push(val)
-    while !@stack_2.empty?
-       @stack.push(@stack_2.pop)
+
+    def enqueue(val)
+        while !@stack.empty?
+           @stack_2.push(@stack.pop)
+        end
+        @stack.push(val)
+        while !@stack_2.empty?
+           @stack.push(@stack_2.pop)
+        end
     end
-  end
 
-  def dequeue
-  @stack.pop
-  end
+    def dequeue
+        @stack.pop
+    end
 
-  def empty?
-    @stack.empty?
-  end
+    def empty?
+      @stack.empty?
+    end
 
-  def size
-    @stack.size
-  end
+    def size
+      @stack.size
+    end
 
-  def peek
-  @stack.first
-  end
+    def peek
+        @stack.first
+    end
 
-  def max 
-    @stack.max
-  end
-  
-  def min
-    @stack.min
-  end
+    def max 
+        @stack.max
+    end
 
-  def diff
-    max - min
-  end
+    def min
+        @stack.min
+    end
+
+    def diff
+        max - min
+    end
 end
 
 def windowed_max_range_with_insane_stackqueue_bullshit(arr, n)
 
-  i = 0
-  check = MinMaxStackQueue.new
-  diff = 0
+    i = 0
+    check = MinMaxStackQueue.new
+    diff = 0
 
-  while (i < arr.length)
-    if check.size == n
-      check.dequeue
-      check.enqueue(arr[i])
-      i += 1
-    else 
-      check.enqueue(arr[i])
-      i += 1
+    while (i < arr.length)
+        if check.size == n
+            check.dequeue
+            check.enqueue(arr[i])
+            i += 1
+        else 
+            check.enqueue(arr[i])
+            i += 1
+        end
+        diff = check.diff if check.diff > diff if check.size == n
     end
-    diff = check.diff if check.diff > diff if check.size == n
-  end
-  return diff
+    return diff
 end
 
 
